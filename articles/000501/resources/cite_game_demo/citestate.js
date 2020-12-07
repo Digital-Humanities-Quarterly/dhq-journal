eb2b50c713de692efc65cacf58e2d22b28565a30
John Walsh <jawalsh@icloud.com>
2020-12-07 16:46:44 -0500

    Updated path to emulators again.

--- articles/000501/resources/cite_game_demo/citestate.js
+++ articles/000501/resources/cite_game_demo/citestate.js
@@ -82,7 +82,7 @@
         var instance;
         var moduleObject = {
             locateFile: function(url) {
-                return window.CiteState.scriptRoot+"resources/emulators/"+url;
+                return window.CiteState.scriptRoot+"resources/cite_game_demo/emulators/"+url;
             },
             targetID:targetID,
             instanceID: window.CiteState.getNextInstanceID(),
@@ -367,7 +367,7 @@
         if (!(emulator in LoadedEmulators) && !(emulator in PendingEmulators)) {
             //Need to track pending to prevent multiple loads of any emulator script if initializing more than one
             PendingEmulators[emulator] = null; //this could be any type of value, just need something there
-            var script = window.CiteState.scriptRoot+"resources/emulators/" + emulator + ".js";
+            var script = window.CiteState.scriptRoot+"resources/cite_game_demo/emulators/" + emulator + ".js";
             //load the script on the page
             var scriptElement = document.createElement("script");
             scriptElement.src = script;
