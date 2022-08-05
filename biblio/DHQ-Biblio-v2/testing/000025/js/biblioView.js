d7b220a2d20559bb0d08c4b938dbf4b873e84e72
Ash Clark <amclark42@gmail.com>
2022-08-05 15:38:53 -0400

    Remove Biblio code as it now has its own repository.
    New repo: https://github.com/Digital-Humanities-Quarterly/biblio

--- biblio/DHQ-Biblio-v2/testing/000025/js/biblioView.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $("button.toggle").click(function()  {
-     $(this).parent().children(".toggle").toggle();
-     $(this).parent().next(".toggle").toggle();
-   });
-});
